89b0d02a5e329d0bc4560840e2edacdb927233d2
Daniel Fischer <danielfm123@gmail.com>
2019-02-16 08:38:32 -0300

    B D dded sync s

---
+++ dataFromS3.sh
@@ -0,0 +1,4 @@
+
+#!/bin/bash
+aws s3 sync s3://danielfm123-public/proyects/salary-gap data --profile danielfm123
+
---
+++ dataToS3.sh
@@ -0,0 +1,4 @@
+
+#!/bin/bash
+aws s3 sync ./data s3://danielfm123-public/proyects/salary-gap --profile danielfm123
+
