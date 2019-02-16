6162d718535df7f11c01c6de6430b1c0da975286
Daniel Fischer <danielfm123@gmail.com>
2019-02-16 16:53:33 +0000

    back to laptop

--- stackoverflow/insight/random_forest.R
+++ stackoverflow/insight/random_forest.R
@@ -51,11 +51,38 @@ salary_fit = h2o.randomForest(y = "ConvertedSalary",
                               training_frame = dataset.h2o,
                               model_id = "salary",
                               ntrees = 500)
-h2o.varimp_plot(salary_fit)
+h2o.saveModel(salary_fit,'data/models/')
+saveRDS(data.frame(h2o.varimp(salary_fit)),'data/models/importance_salary.RDS')
 
 gender_fit = h2o.randomForest(y = "Male",
+                              x = setdiff(colnames(dataset),"Male"),
                               training_frame = dataset.h2o,
                               model_id = "gender",
                               ntrees = 500)
+h2o.saveModel(gender_fit,'data/models/')
+saveRDS(data.frame(h2o.varimp(gender_fit)),'data/models/importance_gender.RDS')
+
+lasso_salary = h2o.glm(y = "ConvertedSalary",
+                      x = setdiff(colnames(dataset),"ConvertedSalary"),
+                      training_frame = dataset.h2o,
+                      model_id = "lasso_salary",
+                      lambda = 0,
+                      remove_collinear_columns = TRUE,
+                      compute_p_values = TRUE,
+                      link = 'identity')
+data.frame(h2o.varimp(lasso_salary))
+arrange(data.frame(lasso_salary@model$coefficients_table),p_value)
+
+lasso_gender = h2o.glm(y = "Male",
+                       x = setdiff(colnames(dataset),"Male"),
+                       training_frame = dataset.h2o,
+                       model_id = "lasso_gender",
+                       lambda = 0,
+                       remove_collinear_columns = TRUE,
+                       compute_p_values = TRUE,
+                       family = 'binomial',
+                       link = 'logit')
+data.frame(h2o.varimp(lasso_gender))
+data.frame(lasso_gender@model$coefficients_table)
 
 h2o.shutdown(prompt = F)
