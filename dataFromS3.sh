7d56e839ad1bfa5895b2cd2fe4681edd891bc674
Daniel Fischer <danielfm123@gmail.com>
2019-02-18 02:36:46 +0000

    1000 arboles

--- dataFromS3.sh
+++ dataFromS3.sh
@@ -1,4 +1,4 @@
 
 #!/bin/bash
-aws s3 sync s3://danielfm123-public/proyects/salary-gap data --profile danielfm123
+aws s3 sync s3://danielfm123-public/proyects/salary-gap data --profile danielfm123 --delete
 
--- stackoverflow/models/random_forest.R
+++ stackoverflow/models/random_forest.R
@@ -12,41 +12,10 @@ dataset.h2o = as.h2o(h2o.aux,destination_frame = "dataset_train")
 salary_fit = h2o.randomForest(y = "ConvertedSalary",
                               x = setdiff(colnames(dataset),"ConvertedSalary"),
                               training_frame = dataset.h2o,
-                              model_id = "salary",
-                              ntrees = 500)
-h2o.saveModel(salary_fit,'data/models/')
+                              model_id = "salary_balanced",
+                              ntrees = 1000)
+h2o.saveModel(salary_fit,'data/models/',force = T)
 saveRDS(data.frame(h2o.varimp(salary_fit)),'data/models/importance_salary.RDS')
 
-gender_fit = h2o.randomForest(y = "Male",
-                              x = setdiff(colnames(dataset),"Male"),
-                              training_frame = dataset.h2o,
-                              model_id = "gender",
-                              ntrees = 500)
-h2o.saveModel(gender_fit,'data/models/')
-saveRDS(data.frame(h2o.varimp(gender_fit)),'data/models/importance_gender.RDS')
-
-lasso_salary = h2o.glm(y = "ConvertedSalary",
-                      x = setdiff(colnames(dataset),"ConvertedSalary"),
-                      training_frame = dataset.h2o,
-                      model_id = "lasso_salary",
-                      lambda = 0,
-                      remove_collinear_columns = TRUE,
-                      compute_p_values = TRUE,
-                      link = 'identity')
-data.frame(h2o.varimp(lasso_salary))
-p_value = arrange(data.frame(lasso_salary@model$coefficients_table),p_value)
-
-
-lasso_gender = h2o.glm(y = "Male",
-                       x = setdiff(colnames(dataset),"Male"),
-                       training_frame = dataset.h2o,
-                       model_id = "lasso_gender",
-                       lambda = 0,
-                       remove_collinear_columns = TRUE,
-                       compute_p_values = TRUE,
-                       family = 'binomial',
-                       link = 'logit')
-data.frame(h2o.varimp(lasso_gender))
-data.frame(lasso_gender@model$coefficients_table)
 
 h2o.shutdown(prompt = F)
